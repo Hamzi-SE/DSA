13304d611b18bb6bf373c995dedad7af501ccc32
Muhammad Hamza <ihamza.se@gmail.com>
2024-08-14 21:11:51 +0500

    Update Remove Array Duplicates.js

--- Remove Array Duplicates.js
+++ Remove Array Duplicates.js
@@ -1,16 +1,14 @@
-const arr = [1, 1, 2, 3, 3, 2, 4, 5, 5, 6];
+const arr = [1, 1, 2, 3, 3, 2, 4, 5, 5, 6, 1];
 
-for (let i = 0; i < arr.length; i++){
+for (let i = 0; i < arr.length; i++) {
     const exists = arr[i];
     
-    for (let j = 1; j < arr.length; j++){
-        
-        if (i === j) continue;
-        
-        if (exists === arr [j]){
+    for (let j = i + 1; j < arr.length; j++) { // Start j at i+1 to avoid comparing the same element
+        if (exists === arr[j]) {
             arr.splice(j, 1);
+            j--; // Adjust j after splice to account for the shifted elements
         }
     }
 }
 
-console.log(arr)
+console.log(arr);
